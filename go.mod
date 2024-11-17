a51a2c99420a0466c2b02db1c5719814f47f436e
Mohammad Hasan Saeedkia <170322053+MHSaeedkia@users.noreply.github.com>
2024-11-17 21:33:47 +0330

    expost complite .

--- go.mod
+++ go.mod
@@ -3,10 +3,25 @@ module github.com/MHSaeedkia/web-crawler
 go 1.23.2
 
 require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/go-sql-driver/mysql v1.7.0 // indirect
+	github.com/google/uuid v1.6.0 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
-	golang.org/x/text v0.14.0 // indirect
+	github.com/mohae/deepcopy v0.0.0-20170929034955-c48cc78d4826 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/richardlehane/mscfb v1.0.4 // indirect
+	github.com/richardlehane/msoleps v1.0.4 // indirect
+	github.com/stretchr/testify v1.9.0 // indirect
+	github.com/xuri/efp v0.0.0-20240408161823-9ad904a10d6d // indirect
+	github.com/xuri/excelize/v2 v2.9.0 // indirect
+	github.com/xuri/nfp v0.0.0-20240318013403-ab9948c2c4a7 // indirect
+	golang.org/x/crypto v0.28.0 // indirect
+	golang.org/x/net v0.30.0 // indirect
+	golang.org/x/text v0.19.0 // indirect
+	gopkg.in/alexcesaro/quotedprintable.v3 v3.0.0-20150716171945-2caba252f4dc // indirect
+	gopkg.in/gomail.v2 v2.0.0-20160411212932-81ebce5c23df // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 	gorm.io/driver/mysql v1.5.7 // indirect
 	gorm.io/gorm v1.25.12 // indirect
 )
--- pkg/export/export.go
+++ pkg/export/export.go
@@ -10,16 +10,21 @@ import (
 	"github.com/MHSaeedkia/web-crawler/internal/models"
 	"github.com/google/uuid"
 	"github.com/xuri/excelize/v2"
+	"gopkg.in/gomail.v2"
 )
 
 const (
 	EXCLE      = 0
 	CSV        = 1
-	fileSystem = "/tmp/"
+	FILESYSTEM = "/tmp/"
+	SMTPHOST   = "smtp.gmail.com"
+	SMTPPORT   = 587
+	MYEMAIL    = "crawlerweb91@gmail.com"
+	MYPASS     = ""
 )
 
-func Csv(report models.Posts, uuid, path string) (string, error) {
-	fileName := fmt.Sprintf("%s/%s.%s", path, uuid /*uuid[:len(string(uuid))-1]*/, "csv")
+func Csv(report []models.Posts, uuid, path string) (string, error) {
+	fileName := fmt.Sprintf("%s/%s.%s", path, uuid, "csv")
 	file, err := os.Create(fileName)
 	if err != nil {
 		return "", err
@@ -34,25 +39,29 @@ func Csv(report models.Posts, uuid, path string) (string, error) {
 		return "", err
 	}
 
-	err = writer.Write(record)
-	if err != nil {
-		return "", err
+	for _, rec := range record {
+		err = writer.Write(rec)
+		if err != nil {
+			return "", err
+		}
+		writer.Flush()
 	}
-	writer.Flush()
 
 	return fileName, nil
 }
 
-func Excle(report models.Posts, uuid, path string) (string, error) {
-	fileName := fmt.Sprintf("%s/%s.%s", path, uuid /*string(uuid)[:len(string(uuid))-1]*/, "xlsx")
+func Excle(report []models.Posts, uuid, path string) (string, error) {
+	fileName := fmt.Sprintf("%s/%s.%s", path, uuid, "xlsx")
 	file := excelize.NewFile()
 	defer file.Close()
 	record, headers := records(report)
 	for i, header := range headers {
 		file.SetCellValue("Sheet1", fmt.Sprintf("%s%d", string(rune(65+i)), 1), header)
 	}
-	for j, col := range record {
-		file.SetCellValue("Sheet1", fmt.Sprintf("%s%d", string(rune(65+j)), 2), col)
+	for j := 0; j < len(record); j++ {
+		for i, rec := range record[j] {
+			file.SetCellValue("Sheet1", fmt.Sprintf("%s%d", string(rune(65+i)), j+2), rec)
+		}
 	}
 
 	if err := file.SaveAs(fileName); err != nil {
@@ -62,7 +71,7 @@ func Excle(report models.Posts, uuid, path string) (string, error) {
 	return fileName, nil
 }
 
-func records(report models.Posts) ([]string, []string) {
+func records(report []models.Posts) ([][]string, []string) {
 	header := []string{
 		"ID", "SrcSitesID", "CitiesID", "UsersID", "Status",
 		"Title", "Description", "Price", "PriceHistory", "MainIMG",
@@ -70,40 +79,41 @@ func records(report models.Posts) ([]string, []string) {
 		"Rooms", "IsApartment", "DealType", "Floors", "Elevator",
 		"Storage", "Location", "PostDate", "DeletedAt", "CreatedAt", "UpdateAt",
 	}
-
-	body := []string{
-		fmt.Sprintf("%v", report.ID),
-		fmt.Sprintf("%v", report.SrcSitesID),
-		fmt.Sprintf("%v", report.CitiesID),
-		fmt.Sprintf("%v", report.UsersID),
-		fmt.Sprintf("%v", report.Status),
-		fmt.Sprintf("%s", report.ExternalSiteID),
-		fmt.Sprintf("%s", report.Title),
-		fmt.Sprintf("%s", report.Description),
-		fmt.Sprintf("%v", report.Price),
-		fmt.Sprintf("%s", report.PriceHistory),
-		fmt.Sprintf("%v", report.MainIMG),
-		fmt.Sprintf("%s", report.GalleryIMGs),
-		fmt.Sprintf("%s", report.SellerName),
-		fmt.Sprintf("%v", report.LandArea),
-		fmt.Sprintf("%v", report.BuiltYear),
-		fmt.Sprintf("%v", report.Rooms),
-		fmt.Sprintf("%t", report.IsApartment),
-		fmt.Sprintf("%v", report.DealType),
-		fmt.Sprintf("%v", report.Floors),
-		fmt.Sprintf("%t", report.Elevator),
-		fmt.Sprintf("%t", report.Storage),
-		fmt.Sprintf("%s", report.Location),
-		fmt.Sprintf("%s", report.PostDate.String()),
-		fmt.Sprintf("%s", report.DeletedAt.String()),
-		fmt.Sprintf("%s", report.CreatedAt.String()),
-		fmt.Sprintf("%s", report.UpdateAt.String()),
+	var body [][]string
+	for _, post := range report {
+		body = append(body, []string{
+			fmt.Sprintf("%v", post.ID),
+			fmt.Sprintf("%v", post.SrcSitesID),
+			fmt.Sprintf("%v", post.CitiesID),
+			fmt.Sprintf("%v", post.UsersID),
+			fmt.Sprintf("%v", post.Status),
+			fmt.Sprintf("%s", post.ExternalSiteID),
+			fmt.Sprintf("%s", post.Title),
+			fmt.Sprintf("%s", post.Description),
+			fmt.Sprintf("%v", post.Price),
+			fmt.Sprintf("%s", post.PriceHistory),
+			fmt.Sprintf("%v", post.MainIMG),
+			fmt.Sprintf("%s", post.GalleryIMGs),
+			fmt.Sprintf("%s", post.SellerName),
+			fmt.Sprintf("%v", post.LandArea),
+			fmt.Sprintf("%v", post.BuiltYear),
+			fmt.Sprintf("%v", post.Rooms),
+			fmt.Sprintf("%t", post.IsApartment),
+			fmt.Sprintf("%v", post.DealType),
+			fmt.Sprintf("%v", post.Floors),
+			fmt.Sprintf("%t", post.Elevator),
+			fmt.Sprintf("%t", post.Storage),
+			fmt.Sprintf("%s", post.Location),
+			fmt.Sprintf("%s", post.PostDate.String()),
+			fmt.Sprintf("%s", post.DeletedAt.String()),
+			fmt.Sprintf("%s", post.CreatedAt.String()),
+			fmt.Sprintf("%s", post.UpdateAt.String()),
+		})
 	}
-
 	return body, header
 }
 
-func Export(report models.Posts, method int) (string, error) {
+func Export(report []models.Posts, method int) (string, error) {
 	var (
 		uid      uuid.UUID
 		fileName string
@@ -113,9 +123,9 @@ func Export(report models.Posts, method int) (string, error) {
 	uid = uuid.New()
 	switch method {
 	case CSV:
-		fileName, err = Csv(report, uid.String(), fileSystem)
+		fileName, err = Csv(report, uid.String(), FILESYSTEM)
 	case EXCLE:
-		fileName, err = Excle(report, uid.String(), fileSystem)
+		fileName, err = Excle(report, uid.String(), FILESYSTEM)
 	}
 	return fileName, err
 }
@@ -172,8 +182,14 @@ func ZipExport(pahts []string, path string) (string, error) {
 }
 
 func EmailExport(email, path string) error {
-
-	return nil
+	msg := gomail.NewMessage()
+	msg.SetHeader("From", MYEMAIL)
+	msg.SetHeader("To", email)
+	msg.SetHeader("Subject", "Web crawler")
+	msg.Attach(path)
+	dial := gomail.NewDialer(SMTPHOST, SMTPPORT, MYEMAIL, MYPASS)
+	err := dial.DialAndSend(msg)
+	return err
 }
 
 func DeleteExport(pahts []string) error {
