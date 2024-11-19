597589d9db5fa67e77d1735f197ec9b0d2f40037
Amin_Esmaili <aminesmaili1380@gmail.com>
2024-11-19 04:09:51 +0330

    base for update_price_crawler.go

--- new_structure/modules/web-crawler/Lib/divar/update_price_crawler.go
+++ new_structure/modules/web-crawler/Lib/divar/update_price_crawler.go
@@ -1,38 +1,41 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"time"
 
-	"github.com/MHSaeedkia/web-crawler/pkg/config"
 	"github.com/MHSaeedkia/web-crawler/cmd/web-crawler/utils"
+	"github.com/MHSaeedkia/web-crawler/pkg/config"
 	"github.com/chromedp/chromedp"
-	"gorm.io/gorm"
 )
 
 // Post represents the structure of the 'posts' table
 type Post struct {
-	ID           uint   `gorm:"primaryKey"` // Primary key
+	ID             uint `gorm:"primaryKey"` // Primary key
 	ExternalSiteID string
-	Price        int
-	PriceHistory string
+	Price          int
+	PriceHistory   string
+	IsApartment    bool
+	DealType       int
 }
 
 // ScrapeAndCheckPrice function as previously implemented
 func ScrapeAndCheckPrice(link string, givenPrice int, placeType bool, contractType int) (int, error) {
 	ctx, cancel := chromedp.NewContext(context.Background())
 	defer cancel()
-	titleSelector := "#app > div.container--has-footer-d86a9.kt-container > div > main > article > div > div.kt-col-5 > section:nth-child(1) > div.kt-page-title > div > h1"
+
 	var elements []string
-	var scrapedPriceText string
+	var tempPrice string
 	var price int
 
 	ctx, cancel = context.WithTimeout(ctx, 30*time.Second)
 	defer cancel()
+
 	err := chromedp.Run(ctx,
 		chromedp.Navigate(link),
-		chromedp.WaitVisible(titleSelector, chromedp.ByQuery),
+		chromedp.WaitVisible(`#app > div.container--has-footer-d86a9.kt-container`, chromedp.ByQuery),
 		chromedp.Evaluate(`Array.from(document.querySelectorAll("p.kt-unexpandable-row__value")).map(e => e.textContent.trim())`, &elements),
 	)
 	if err != nil {
@@ -40,68 +43,66 @@ func ScrapeAndCheckPrice(link string, givenPrice int, placeType bool, contractTy
 		return 0, fmt.Errorf("failed to scrape the link: %w", err)
 	}
 
-	if !placeType { // Villa
-		if contractType == 1 { // Buy
+	// Extract price logic here based on `placeType` and `contractType`
+	if placeType { //apartement
+		tempPrice = elements[0]
+		price = utils.ConvertToInt(tempPrice)
+	} else {
+		// Villa logic
+		if contractType == 1 {
 			if len(elements) == 3 {
-				TempPrice = elements[1]
+				tempPrice = elements[1]
 			} else if len(elements) > 3 {
-				TempPrice = elements[2]
+				tempPrice = elements[2]
 			}
-			Price = utils.ConvertToInt(TempPrice)
-		} else { // Rent
+			price = utils.ConvertToInt(tempPrice)
+		} else {
 			if len(elements) == 4 {
-				TempDesposit = elements[1]
+				tempPrice = elements[0]
+				price = utils.ConvertToInt(tempPrice)
 			} else {
-				return 0, fmt.Errorf("invalid data structure")
+				return 0, nil
 			}
-			Price = utils.ConvertToInt(TempDesposit)
 		}
-	} else { // Apartment
-		if contractType == 1 { // Buy
-			TempPrice = elements[0]
-			Price = utils.ConvertToInt(TempPrice)
-		} else { // Rent
-			if len(elements) == 4 {
-				TempDesposit = elements[0]
-			} else {
-				return 0, fmt.Errorf("invalid data structure")
-			}
-			Price = utils.ConvertToInt(TempDesposit)
-		}
+
 	}
 
-	if Price == givenPrice {
-		return 0, nil
+	if price == givenPrice {
+		return 0, nil // No change
 	}
-	return Price, nil
+	return price, nil // Return new price if changed
 }
-
 
 func updatePriceCrawler() {
 	// Connect to the database
-	db, err := config.ConnectDB() // Assumes config.ConnectDB() is implemented as in previous examples
+	db, err := config.ConnectDB()
 	if err != nil {
 		log.Fatal("Error connecting to the database:", err)
 		return
 	}
 	defer db.Close()
 
-	// Fetch all ExternalSiteID values from the posts table
+	// Fetch all posts with ExternalSiteID and Price
 	var posts []Post
-	if err := db.Select("external_site_id").Find(&posts).Error; err != nil {
-		log.Fatal("Failed to fetch ExternalSiteID from posts:", err)
+	if err := db.Select("id, external_site_id, price, price_history").Find(&posts).Error; err != nil {
+		log.Fatal("Failed to fetch posts:", err)
 		return
 	}
 
-	// Extract ExternalSiteID into a list
 	for _, post := range posts {
-		link := post.ExternalSiteID
+		// Construct the link from the ExternalSiteID
+		link := fmt.Sprintf("https://divar.ir/v/%s", post.ExternalSiteID)
+		placetype := post.IsApartment
+		contractType := post.DealType
 
-		newPrice, err := ScrapeAndCheckPrice(link, post.Price, post.IsApartment, post.DealType) // Example: Assume placeType=true, contractType=1
+		// Scrape the current price
+		newPrice, err := ScrapeAndCheckPrice(link, post.Price, placetype, contractType) // Example: Assume placeType=true, contractType=1
 		if err != nil {
 			log.Printf("Failed to scrape ExternalSiteID %s: %v\n", post.ExternalSiteID, err)
 			continue
 		}
+
+		// If the price has changed, update the database
 		if newPrice != 0 {
 			// Update price history
 			newPriceHistory := fmt.Sprintf("%s;%d:%s", post.PriceHistory, post.Price, time.Now().Format("2006-01-02 15:04:05"))
@@ -118,7 +119,6 @@ func updatePriceCrawler() {
 		}
 	}
 }
-
 
 func main() {
 	updatePriceCrawler()
